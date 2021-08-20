201ca5071ba5b066ece8220b5b5706c9406c3236
AdriJordant <jordan_adri_rojas@hotmail.com>
2021-08-19 23:26:49 -0500

    Add files via upload

---
+++ components/contador.js
@@ -0,0 +1,26 @@
+const Contador=()=>{
+    /*const [contador,setContador]=React.useState(0);
+    const aumentar=()=>setContador(contador+1);
+    const disminuir=()=>setContador(contador-1);*/
+    //console.log(contador);
+    alert("Click en  ''aceptar''");
+    return (
+    <div>
+        <h1>Esto no es un contador pero puedes contar conmigo ♥</h1>
+        <hr />
+        <br></br>
+        <br></br>
+        <br></br>
+        
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/zwOUt4mMt8U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+        
+        <br></br>
+        <br></br>        
+    </div>
+
+    )
+}
